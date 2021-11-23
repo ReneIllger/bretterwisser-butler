d2ef1b4192fd19187382d9462662616f537c2626
René Illger <rene.illger@mvise.de>
2021-11-23 15:49:57 +0100

    first commit

---
+++ src/main/java/de/bretterwisser/butler/BretterwisserButlerApplication.java
@@ -0,0 +1,13 @@
+package de.bretterwisser.butler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BretterwisserButlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BretterwisserButlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/bretterwisser/butler/BretterwisserButlerApplicationTests.java
@@ -0,0 +1,12 @@
+package de.bretterwisser.butler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BretterwisserButlerApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
